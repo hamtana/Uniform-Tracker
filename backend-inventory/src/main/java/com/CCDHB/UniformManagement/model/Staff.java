991e0b08ab7365b5306d720df114ae2f0886d3c3
--get <phiha428@student.otago.ac.nz>
2025-07-23 12:19:09 +1200

    added datafields to staff model, references #9

--- backend-inventory/src/main/java/com/CCDHB/UniformManagement/model/Staff.java
+++ backend-inventory/src/main/java/com/CCDHB/UniformManagement/model/Staff.java
@@ -1,4 +1,14 @@
 package com.CCDHB.UniformManagement.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Staff {
+
+    @Id
+    private String email;
+
+    private String name;
+    // Add reference to Manager.
 }
