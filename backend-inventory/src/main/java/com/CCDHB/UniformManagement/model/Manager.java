3edc2e56c91d147c97bc4e40afd5ed481eac8591
--get <phiha428@student.otago.ac.nz>
2025-07-31 09:51:11 +1200

    create manager model, closes  #11

--- backend-inventory/src/main/java/com/CCDHB/UniformManagement/model/Manager.java
+++ backend-inventory/src/main/java/com/CCDHB/UniformManagement/model/Manager.java
@@ -1,4 +1,38 @@
 package com.CCDHB.UniformManagement.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Manager {
+
+    @Id
+    private int managerId;
+
+    private String email;
+    private String name;
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
