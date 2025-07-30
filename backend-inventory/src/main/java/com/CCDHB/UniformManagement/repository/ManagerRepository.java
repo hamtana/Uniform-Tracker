093d2bc54ede4ddc4b9878288d2ce87af4a41382
--get <phiha428@student.otago.ac.nz>
2025-07-31 11:26:09 +1200

    created manager repository interface, closes #15

---
+++ backend-inventory/src/main/java/com/CCDHB/UniformManagement/repository/ManagerRepository.java
@@ -0,0 +1,8 @@
+package com.CCDHB.UniformManagement.repository;
+
+import com.CCDHB.UniformManagement.model.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+}
